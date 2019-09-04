57c4e63f15cd65e714ce7435f31159215ac565bc
Axel Neergaard <axel.neergaard@aalto.fi>
2019-09-04 23:08:09 +0300

    Set lets as consts

--- info0/index.php
+++ info0/index.php
@@ -77,8 +77,8 @@
 
         function getUrlParameter(name) {
             name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-            let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-            let results = regex.exec(location.search);
+            const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+            const results = regex.exec(location.search);
             return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
         };
 
